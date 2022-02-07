c2ab446ceb4eedaf81688d538c0786216c956bcd
Shridhar-dev <shridharkamat10@gmail.com>
2022-02-07 21:13:14 +0530

    First one

---
+++ components/Button.js
@@ -0,0 +1,10 @@
+
+function Button() {
+  return(
+    <div className="text-xl mt-10 mb-20 py-4 px-8 border-2 rounded">
+        Start Exploring ✨
+    </div>
+  );
+}
+
+export default Button;
---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+//  mumbai: {
+//    url: "",
+//    accounts: 
+//  }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
